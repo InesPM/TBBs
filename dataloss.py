ec2e2434924533cd6d4a6d18decf6b404548ddac
Inés Pastor-Marazuela <29770213+InesPM@users.noreply.github.com>
2020-05-12 18:41:31 +0200

    Now works on list of files

--- dataloss.py
+++ dataloss.py
@@ -1,35 +1,69 @@
+from __future__ import print_function
+from __future__ import division
 import sys
+import glob
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+from optparse import OptionParser
 
-fn = sys.argv[1]
-#station = sys.argv[2]
-#title = sys.argv[3]
+def dataloss(fn):
+    #path = '/data/projects/COM_ALERT/pipeline/analysis/marazuela/data/'
+    #out = path + fn.split('/')[-1].replace('.h5', '.png')
+    #title = fn.split('/')[-1].replace('.h5', '')
 
-path = '/data/projects/COM_ALERT/pipeline/analysis/marazuela/data/'
-out = path + fn.split('/')[-1].replace('.h5', '.png')
-title = fn.split('/')[-1].replace('.h5', '')
+    ff = h5py.File(fn)
 
-ff = h5py.File(fn)
+    station = str(ff.keys()[0])
+    dipoles = ff[station].keys()
 
-station = str(ff.keys()[0])
+    nzeros  = 0
+    datalen = 0
 
-data_arr = [ff[station][kk][jj][:].real 
-           for kk in ff[station].keys() 
-           for jj in ff[station][kk].keys()]
-data_arr = np.concatenate(data_arr)
+    #data_arr = [ff[station][kk][jj][:].real for kk in ff[station].keys() 
+    #        for jj in ff[station][kk].keys()]
+    #data_arr = np.concatenate(data_arr)
 
-zeros = np.load('/home/marazuela/scripts/zeros.npy')
-#print "# zeros",np.sum((data_arr['real']==0)&(data_arr['imag']==0)),"out of",len(data_arr)," dataloss ",round(100.0*np.sum((data_arr['real']==0)&(data_arr['imag']==0))/len(data_arr),1),"%"
-#print "# zeros",np.sum((data_arr==zeros)),"out of",len(data_arr)," dataloss ",round(100.0*np.sum((data_arr==zeros))/len(data_arr),1),"%"
-print round(100.0*np.sum((data_arr==zeros))/len(data_arr),1)
+    zeros = np.array((0, 0), dtype=[('real', '<i2'), ('imag', '<i2')])
+    #np.load('/home/marazuela/scripts/zeros.npy')
 
-# Plots
+    for d in dipoles:
+        subbands = ff[station][d].keys()
+        for sb in subbands:
+            datalen += len(ff[station][d][sb][:])
+            nzeros  += len(np.where(ff[station][dipoles[0]][subbands[0]][:]==zeros)[0])
 
-#plt.figure(figsize=(21, 8))
-#plt.plot(data_arr, color='teal')
-#plt.xlabel('Time sample')
-#plt.savefig(out, pad_inches=0, bbox_inches='tight')
-#plt.show()
+    dataloss = round(100.0 * nzeros / datalen, 1)
+
+    return dataloss
+
+if __name__=='__main__':
+    
+    # Command line arguments
+    parser = OptionParser()
+    (options, args) = parser.parse_args()
+
+    # Input files
+    files = []
+    stations = []
+    for a in args[:]:
+        f = glob.glob(a)[0]
+        files.append(f)
+        stations.append(f.split('/')[-1].split('_')[1])
+
+    files.sort()
+    stations = np.unique(stations)
+    stations.sort()
+
+    for s in stations:
+        print('\n', s, end='\t')
+        for f in files:
+            if s in f:
+                dl = dataloss(f)
+                print(dl, end='\t')           
+
+
+
+
+
 
